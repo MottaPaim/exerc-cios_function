2c907145f3ce0f3a6bc94fa10c8bcfb5ecdc402b
Felipe <124448047+MottaPaim@users.noreply.github.com>
2024-02-28 01:04:49 -0300

    Update index.js

--- Function_Alert/index.js
+++ Function_Alert/index.js
@@ -1,5 +1,5 @@
-function exibirMensagemDeBoasVindas() {
-    // para executar uma function usar parenteses vazios
+function exibirMensagemDeBoasVindas() {  // para executar uma function usar parenteses vazios
+                                        
     alert("Bem vindo Senhor.");
 }
 
